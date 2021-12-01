7a2a478d37db424043b5c48c04cfc27e48bce1ed
Yulia <suzdaleva22@gmail.com>
2021-12-01 10:44:56 +0200

    Homework1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,143 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div v-if="showForm" class="form">
+      <div class="formitem">
+        <legend>Email</legend>
+        <input type="text" v-model="userInfo.email" />
+      </div>
+      <div class="formitem">
+        <legend>First Name</legend>
+        <input type="text" v-model="userInfo.firstName" />
+      </div>
+      <div class="formitem">
+        <legend>Last Name</legend>
+        <input type="text" v-model="userInfo.lastName" />
+      </div>
+      <div class="formitem">
+        <legend>Phone</legend>
+        <input type="text" v-model="userInfo.phone" />
+      </div>
+      <hr />
+      <div class="guests">
+        <span class="bolder">Guests</span>
+        <button class="btn btn-submit" @click="addGuest">+</button>
+         <input class="form-guest"
+          v-for="(guest, index) in guests"
+          type="text"
+          v-model="guests[index]"
+          :key="index"
+        />
+      </div>
+      <hr />
+      <button class="btn btn-success" @click="sendData">Send Data</button>
+      <hr />
+    </div>
+    <div v-else>
+      <h2 class="h2-center">All Done!</h2>
+      <table class="table table-bordered">
+        <tr>
+          <td>Email</td>
+          <td>{{ userInfo.email }}</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>{{ userInfo.firstName }} {{ userInfo.lastName }}</td>
+        </tr>
+        <tr>
+          <td>Phone</td>
+          <td>{{ userInfo.phone }}</td>
+        </tr>
+        <tr>
+          <td>Guests</td>
+          <td>
+            <ul class="list-group">
+              <li
+                class="group-item"
+                v-for="(guest, index) in guests"
+                :key="index"
+              >
+                {{ guest }}
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </table>
+      <button class="btn btn-success back" @click="back">Back</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      userInfo: {
+        email: "",
+        firstName: "",
+        lastName: "",
+        phone: "",
+      },
+      guests: [],
+      newGuest: "",
+      showForm: true,
+    };
+  },
+  methods: {
+    addGuest() {
+      this.showInput = true;
+      this.guests.push("");
+    },
+    sendData() {
+      this.showForm = false;
+    },
+    back() {
+       this.showForm = true;
+    }, 
+   }
+ };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+legend {
+  font-size: 1rem;
+  margin-bottom: 0;
+  font-weight: 600;
+}
+button.back {
+    display: block;
+    margin-left: auto;
+    margin-right: auto }
+
+button.btn-submit {
+  margin-left: 20px;
+}
+.table {
+  margin: 0 auto;
+  width: 350px !important;
+}
+.form-guest {
+  margin-top: 10px;
+}
+.bolder {
+  font-weight: 600;
+}
+.form {
+  margin: 0 auto;
+  width: 350px;
+  margin-top: 50px;
+}
+.formitem {
+  margin-bottom: 10px;
+}
+.h2-center {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+input {
+  margin: 0 auto;
+  width: 350px;
+}
+.list-group {
+  margin-left: 20px;
 }
 </style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="test-class" :style="{ background: color }">test component</div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "tomato",
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.test-class {
+  width: 100%;
+  height: 100vh;
+  border: 2px solid salmon;
+}
+</style>
